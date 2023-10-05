607b7aff83276940a0aa8434c3220fe3825aaad3
Ashraful25 <65948706+Ashraful25@users.noreply.github.com>
2023-10-06 02:35:08 +0600

    Update layout.tsx

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -17,7 +17,7 @@ const DashboardLayout = async (
         <div className="h-full relative">
             <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 bg-gray-900">
             
-                <Sidebar  apiLimitCount = {apiLimitCount}/>
+                <Sidebar  apiLimitCount = {apiLimitCount} onClose= null />
                 
             </div>
             <main className="md:pl-72">
